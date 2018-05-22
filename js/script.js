123445770af3a8b64e12d06ae35a0e7ff03da822
Konrad Ryczek <konradryczek92@gmail.com>
2018-05-22 12:28:01 +0200

    Add script.js content

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var a = prompt('Podaj a'),
+	b = prompt('Podaj b'),
+	value = (a * a) - (2 * a * b) - (b * b);
+console.log('Wynik to: ' + value);
+if (value > 0){
+	console.log('Wynik dodatni');
+} else if (value < 0){
+	console.log('Wynik ujemny');
+} else {
+	console.log('Wynik równy 0');
+}
